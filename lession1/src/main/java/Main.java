f859f838545512af5a42a90eed48e5a74b7393ed
sitoff <svlsitoff@gmail.com>
2021-08-27 23:28:20 +0800

    little check comments

--- lession1/src/main/java/Main.java
+++ lession1/src/main/java/Main.java
@@ -15,8 +15,11 @@ public class Main {
 
     public static void main(String ... args)  {
 
+        //loading context
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/spring-context.xml");
+        //create service
         QuizService service = context.getBean(QuizServiceImpl.class);
+        //from service get list quiz
         for(Quiz quiz : service.getAllQuestions()){
             System.out.println(quiz.getQuestion());
             for(String item : quiz.getOptions()){
